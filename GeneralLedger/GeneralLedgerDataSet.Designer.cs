883cc5d49df22e7bd3e2dae66b281ae4be28509c
Elgene Quiamco <Elgene@qualiticks.com>
2026-05-20 14:20:38 +0800

    05-20-2026

--- GeneralLedger/GeneralLedgerDataSet.Designer.cs
+++ GeneralLedger/GeneralLedgerDataSet.Designer.cs
@@ -19336,7 +19336,7 @@ namespace GeneralLedger.GeneralLedgerDataSetTableAdapters {
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
         private void InitConnection() {
             this._connection = new global::System.Data.SqlClient.SqlConnection();
-            this._connection.ConnectionString = global::GeneralLedger.Properties.Settings.Default.GeneralLedgerConnectionString1;
+            this._connection.ConnectionString = global::GeneralLedger.Properties.Settings.Default.GeneralLedgerConnectionString;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
